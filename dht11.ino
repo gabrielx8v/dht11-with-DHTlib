e448bb6a6832b75249b870c09259bd3ce096de81
Gabriel Schnorrenberger <gschnorrenberger@yahoo.com>
2019-08-17 20:07:09 -0300

    Create dht11.ino

---
+++ dht11.ino
@@ -0,0 +1,27 @@
+#include <dht.h> // Inclui a biblioteca dht h no seu código
+ 
+dht DHT; // Cria um objeto da classe dht
+ 
+void setup()
+{
+  Serial.begin(9600); // Inicializa serial com taxa de transmissão de 9600 bauds
+}
+
+void loop()
+{ 
+  //faz leitura do dht11
+  {
+    DHT.read11(A0); // chama método de leitura da classe dht na porta analogica especificada,
+                    // com o pino de transmissão de dados ligado no pino A0(porta analogica especificada);
+    
+    // Exibe na serial o valor de umidade
+    Serial.print(DHT.humidity);
+    Serial.println(" %");
+    
+    // Exibe na serial o valor da temperatura
+    Serial.print(DHT.temperature);
+    Serial.println(" Celsius");
+    
+    delay(3000); //pausa de 3 segundos antes da proxima leitura
+  }
+}
